8e6eb07b6e169a935507243ffd6c387f17d73072
jrejoire <jrejoire@hotmail.fr>
2020-10-27 01:39:22 +0100

    minor fix

--- index.js
+++ index.js
@@ -8,12 +8,6 @@ require('dotenv').config();
 const port = process.env.PORT || 5000;
 
 const app = express();
-app.use(express.json());
-var corsOptions = {
-    origin: '*',
-    credentials: true
-};
-app.use(cors(corsOptions));
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "https://jrejoire.github.io");
     res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
@@ -21,6 +15,12 @@ app.use((req, res, next) => {
     res.header("Access-Control-Allow-Credentials", true);
     next();
 });
+app.use(express.json());
+var corsOptions = {
+    origin: '*',
+    credentials: true
+};
+app.use(cors(corsOptions));
 
 var server = app.listen(port, () => console.log(`Listening to server ${port}`));
 
